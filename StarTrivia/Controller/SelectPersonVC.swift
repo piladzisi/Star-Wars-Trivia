85f040dc43b167ad62ae0a9be7db654f164ce567
Anna <piladzisi@gmail.com>
2019-05-07 16:25:06 +0300

    JSON serialization

--- StarTrivia/Controller/SelectPersonVC.swift
+++ StarTrivia/Controller/SelectPersonVC.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class SelectPersonVC: UIViewController {
 
+    var personApi = PersonApi()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       personApi.getRandomPersonUrlSession()
     }
 
 
