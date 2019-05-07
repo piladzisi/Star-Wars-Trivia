45e26bbda26d6b8fe377ac2a0bff4d0ac0bfcd10
Anna <piladzisi@gmail.com>
2019-05-07 10:01:09 +0300

    UI

---
+++ StarTrivia/Controller/SelectPersonVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by anna.sibirtseva on 07/05/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class SelectPersonVC: UIViewController {
+
+    @IBOutlet weak var stackBackground: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        stackBackground.layer.cornerRadius = 10
+    }
+
+
+}
+
