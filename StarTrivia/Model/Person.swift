ec05100b01f10f1b264ea328edbeabe3092b9f95
Anna <piladzisi@gmail.com>
2019-05-08 17:36:21 +0300

    with Codable

--- StarTrivia/Model/Person.swift
+++ StarTrivia/Model/Person.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-struct Person {
+struct Person: Codable {
     let name: String
     let height: String
     let mass: String
@@ -21,4 +21,17 @@ struct Person {
     let vehicleUrls: [String]
     let starshipUrls: [String]
     
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case height
+        case mass
+        case hair = "hair_color"
+        case birthYear = "birth_year"
+        case gender
+        case homeworldUrl = "homeworld"
+        case filmUrls = "films"
+        case vehicleUrls = "vehicles"
+        case starshipUrls = "sharships"
+    }
 }
--- StarTrivia/Services/PersonApi.swift
+++ StarTrivia/Services/PersonApi.swift
@@ -8,8 +8,58 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class PersonApi {
+    
+    
+    //Web request with Alamofire and Codable
+    
+    func getRandomPersonAlamofireCodable(id: Int, completion: @escaping PersonResponseCompletion) {
+        
+        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
+        Alamofire.request(url).responseJSON { (response) in
+            
+            if let error = response.result.error {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            guard let data = response.data else { return completion(nil)}
+            let jsonDecoder = JSONDecoder()
+            do {
+                let person = try jsonDecoder.decode(Person.self, from: data)
+                completion(person)
+            } catch {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+            }
+        }
+   }
+    
+    //Web request with Alamofire and SwiftyJson
+    func getRandomPersonAlamofireSwiftyJson(id: Int, completion: @escaping PersonResponseCompletion) {
+        
+        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
+        Alamofire.request(url).responseJSON { (response) in
+            
+            if let error = response.result.error {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            guard let data = response.data else { return completion(nil)}
+            do {
+                let json = try JSON(data: data)
+                let person = self.parsePersonSwifty(json: json)
+                completion(person)
+            } catch {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+            }
+       }
+  }
+    
     
     //Web request with Alamofire
     func getRandomPersonAlamofire(id: Int, completion: @escaping PersonResponseCompletion) {
@@ -62,6 +112,26 @@ class PersonApi {
         
     }
     
+    //parsing  with SwiftyJson
+    private func parsePersonSwifty (json: JSON) -> Person {
+        let name = json["name"].stringValue
+        let height = json["height"].stringValue
+        let mass = json["mass"].stringValue
+        let hair = json["hair_color"].stringValue
+        let birthYear   = json["birth_year"].stringValue
+        let gender = json["gender"].stringValue
+        let homeWorldUrl = json["homeworld"].stringValue
+        let filmUrls = json["films"].arrayValue.map({$0.stringValue})
+        let vehicleUrls = json["vehicles"].arrayValue.map({$0.stringValue})
+        let starshipUrls = json["starships"].arrayValue.map({$0.stringValue})
+        
+        return Person(name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeworldUrl: homeWorldUrl
+            , filmUrls: filmUrls, vehicleUrls: vehicleUrls, starshipUrls: starshipUrls)
+        
+    }
+    
+    
+    //parsing JSON manually
     private func parsePersonManual(json: [String: Any]) -> Person {
         let name = json["name"] as? String ?? ""
         let height = json["height"] as? String ?? ""
